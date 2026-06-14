ff9b78635607fcc16ad9a950748529f789266bdc
Madeline Barnes <madelinebarnes0706@gmail.com>
2026-06-14 13:15:33 -0700

    Clean up

--- configuration.nix
+++ configuration.nix
@@ -1,33 +1,42 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+# Dell Pro 14 NixOS config
 
 { config, lib, pkgs, ... }:
-
 {
   programs.sway.enable = true;
   services.displayManager.ly.enable = true;
   environment.systemPackages = with pkgs; [
     waybar
-    git
     fastfetch
+    git
   ];
-
-
   imports =
     [
       ./hardware-configuration.nix
     ];
-
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   networking.hostName = "arcanine-nix";
   networking.networkmanager.enable = true;
   time.timeZone = "America/Los_Angeles";
 
-  # User
+  # I am lazy
+  programs.bash.interactiveShellInit = ''
+    nixpush() {
+      cd ~/nixos-config && \
+      git add . && \
+      git commit -m "''${1:-Update config}" && \
+      git push
+    }
+  # Lazy part 2
+    nixup() {
+      cd ~/nixos-config && \
+      sudo nixos-rebuild switch && \
+      git add . && \
+      git commit -m "''${1:-Update config}" && \
+      git push
+    }
+  '';
+  # Hey hey thats me
   users.users.madeline = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
@@ -35,10 +44,7 @@
       tree
     ];
   };
-
+  # The best browser
   programs.firefox.enable = true;
-
   system.stateVersion = "26.05";
-
 }
-
